2df9b1aec2e65958f831226f0e88b2f4d6e6449b
Syed Sumail Azhar <78620824+Sumail-CALAMITY@users.noreply.github.com>
2024-01-20 22:49:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+alert("Welcome to Our Website!\n\n This website is nothing but my javascript project\n\n Thanks for visiting")
+
+var age=("17");
+var isStudent=("I am a student")
+console.log("My age is ", age)
+console.log(isStudent)
---
+++ main2.js
@@ -0,0 +1,13 @@
+var userName=prompt("Enter Your name:","")
+alert("Welcome to our website " + userName)
+
+
+
+
+
+var length=(15)
+var width=(10)
+var area=(length*width)
+console.log("Length=", length)
+console.log("Width=", width)
+console.log("area =", area)
